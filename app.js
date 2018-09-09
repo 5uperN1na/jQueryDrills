06acde8fdaf75c514904bbd828f12b789f71da9a
Paula Suarez <pauladzuck@hotmail.com>
2018-09-09 17:06:24 -0500

    Adding updatest to index, app and readme files

--- app.js
+++ app.js
@@ -1,4 +1,55 @@
-$('#btnSubmit').click(function(){
-    alert (firstName);
+//event handler
+$(document).ready(function () {
+
+
+
+    // $("#submitButton").click(function () {
+    // $("#submitButton").prop('enabled', true);
+    // let fName = $('#firstName').val();
+    // alert(fName);
+
+    // });
+
+
+
+
+    $(function () {
+
+
+        //disable the submit button
+        $('#btnInput').prop('disabled', true);
+
+        $('#txtInput').keyup(function () {
+            if ($(this).val() != '') {
+
+                $('#btnInput').prop('disabled', false);
+
+                $('#btnInput').click(function () {
+
+                    let text = $('#txtInput').val();
+                    alert(text);
+                
+                });
+            }
+
+
+
+        });
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
 
 });
+
